2b923cee1c6eaefc055bcab1c3402743a116e1fb
Irina <irina.schubert@students.ffhs.ch>
2019-11-03 12:46:35 +0100

    cleanup code

--- src/server/server.js
+++ src/server/server.js
@@ -19,43 +19,12 @@ let server = new WebSocketServer({port:port});
  * Establish a connection and add users to room
  * @param {WebSocket} socket
  */
-// Is executed when a new socket connects to the server. Adds user only up until 2 users.
+// Is executed when a new socket connects to the server. Adds user up to 2 users then makes a new room.
 server.on('connection', function(socket, client){
-    addUserToRoom(room1, socket, client);
-    /*
-    else if(room2.users.length < 2){
-        let user = new User(socket);
-        room2.addUser(user);
-        MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
-            if (err) throw err;
-            let dbo = db.db("webEchessDb");
-            let gameRoomUser = { gameRoomId: room2.id, user: user.id };
-            dbo.collection("rooms").insertOne(gameRoomUser, function(err, res) {
-                if (err) throw err;
-                db.close();
-            });
-        });
-        console.log("[Server] A new connection was established. " + user.id + " has joined the game. " +
-            "Total connections in room 2: " + room2.users.length);
-    }
-    else if(room3.users.length < 2 ){
-        let user = new User(socket);
-        room3.addUser(user);
-        MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
-            if (err) throw err;
-            let dbo = db.db("webEchessDb");
-            let gameRoomUser = { gameRoomId: room3.id, user: user.id };
-            dbo.collection("rooms").insertOne(gameRoomUser, function(err, res) {
-                if (err) throw err;
-                db.close();
-            });
-        });
-        console.log("[Server] A new connection was established. " + user.id + " has joined the game. " +
-            "Total connections in room 3: " + room3.users.length);
-    }*/
+    addUserToRoom(room1, socket);
 });
 
-function addUserToRoom(room, socket, client){
+function addUserToRoom(room, socket){
     //let room = new GameRoom();
     if(room.users.length < 2){
         let user = new User(socket);
@@ -74,7 +43,7 @@ function addUserToRoom(room, socket, client){
     }
     else {
         room1 = new GameRoom();
-        addUserToRoom(room1, socket, client);
+        addUserToRoom(room1, socket);
     }
 }
 
